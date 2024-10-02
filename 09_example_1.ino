47888c51844fccc1588375446f50c05cb1332457
sanha0211 <naz.simran.sazia@gmail.com>
2024-10-02 13:56:19 +0900

    09_example_1.ino

--- 09_example_1.ino
+++ 09_example_1.ino
@@ -1,3 +1,4 @@
+
 // Arduino pin assignment
 #define PIN_LED  9
 #define PIN_TRIG 12
@@ -13,7 +14,7 @@
 #define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
 #define SCALE (0.001 * 0.5 * SND_VEL)     // coefficient to convert duration to distance
 
-#define _EMA_ALPHA 0.1    // EMA weight of new sample (range: 0 to 1)
+#define _EMA_ALPHA 0.5    // EMA weight of new sample (range: 0 to 1)
 
 // global variables
 unsigned long last_sampling_time = 0;   // unit: msec
